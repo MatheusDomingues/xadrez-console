a7594ef7e409760e966d5f50170e9370e5796b5a
MatheusDomingues <matheus_luige@hotmail.com>
2022-04-05 16:50:55 -0300

    Testes iniciais com movimentos possiveis das pecas

--- Program.cs
+++ Program.cs
@@ -17,8 +17,14 @@ namespace xadrez_console
           Tela.imprimirTabuleiro(partida.tab);
 
           Console.WriteLine();
-          Console.Write("Oriem: ");
+          Console.Write("Origem: ");
           Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+          bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+          Console.Clear();
+          Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
           Console.Write("Destino: ");
           Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
